db6aae6d4db99321d97529fe204d515610905095
SahuNandan <nandansahu755@gmail.com>
2024-09-18 17:26:43 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import Home from './components/Home'; // Assuming Home contains the sections with IDs
 import PageNotFound from './components/PageNotFound';
